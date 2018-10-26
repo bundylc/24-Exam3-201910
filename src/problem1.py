46cf36e095a9cb2ce28891bb74307fae0bf5d280
bundylc <bundy@rose-hulman.edu>
2018-10-26 08:54:07 -0400

    done with m1

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Exam 3, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Landon Bundy.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # TODdO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -68,7 +68,15 @@ def run_test_problem1():
 
 
 def problem1(numbers):
-    """
+    length = len(numbers)
+    length = length // 3
+    yo = 0
+    for k in range(length):
+        now = numbers[k+length]
+        yo = yo + now
+    return yo
+
+"""
     What comes in:
       -- A non-empty sequence of numbers, with the sequence having a length
             that is evenly divisible by 3
@@ -86,7 +94,7 @@ def problem1(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # TODOd: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
